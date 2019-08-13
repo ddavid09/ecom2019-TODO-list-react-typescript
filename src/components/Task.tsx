24f47d46a32c72393a39fe2b708b356132f170e8
ddavid09 <ddawidziak.dd@gmail.com>
2019-08-13 11:55:24 +0200

    dodanie przyciskow spec taskow i ich stylow

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -4,6 +4,7 @@ import '../App.css';
 
 interface ITaskState {
     done: boolean;
+    isMouseInside: boolean;
 }
 
 interface TaskComponentProps extends TaskModel {
@@ -15,7 +16,7 @@ export class Task extends Component<TaskComponentProps, ITaskState> {
     constructor(props: TaskComponentProps) {
         super(props);
 
-        this.state = { done: this.props.done };
+        this.state = { done: this.props.done, isMouseInside: false };
         console.log("utworzono nowego taska: #" + this.props.id + " " + this.props.text);
     }
 
@@ -26,29 +27,47 @@ export class Task extends Component<TaskComponentProps, ITaskState> {
         this.props.markAsDoneFunc(this.props.id);
     }
 
+    mouseEnter = () => {
+        this.setState({ isMouseInside: true });
+    }
+    mouseLeave = () => {
+        this.setState({ isMouseInside: false });
+    }
+
 
     render() {
         let color: string;
         let text: any;
         let textColor: string;
-        if (this.state.done){
+        if (this.state.done) {
             color = '#96968f';
             text = <s>{this.props.text}</s>;
             textColor = "#696964";
-        } 
-        else{
+        }
+        else {
             color = '#ffff61';
             text = this.props.text;
             textColor = "#000000";
-        } 
+        }
+
 
         return (
-            <div className='taskbtn'>
-                <button onClick={this.onClick} style={{ backgroundColor: color, color: textColor }}>
-                    <p>{text}</p>
-                </button>
+            <div
+                className='taskbtn'
+                onClick={this.onClick}
+                style={{ backgroundColor: color, color: textColor }}
+                onMouseEnter={this.mouseEnter}
+                onMouseLeave={this.mouseLeave}
+            >
+                {this.state.isMouseInside ? 
+                <div className='taskbtn-overlay'>
+                    <button className='overlay-button-delete'>Delete</button>
+                    <button className='overlay-button-mark'>Mark up</button>
+                </div> : null}
+                <p>{text}</p>
             </div>
         )
+
     }
 }
 
